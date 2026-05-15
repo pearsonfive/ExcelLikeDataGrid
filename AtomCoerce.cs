632ad4d1573aa26990e64b2445c9c6e0e142458d
Simon Pearson <work@simonpearson.com>
2026-05-15 18:03:53 +0100

    Create AtomCoerce.cs

---
+++ AtomCoerce.cs
@@ -0,0 +1,102 @@
+public static XlCall.XlReturn AtomXlCoerce(out object result, object input, XlType xlType)
+{
+    // Atom implementation of the coercion in Excel. Allows Atom to avoid calling Excel
+    // in background threads.
+
+    // Replacement for:
+    // public static XlReturn TryExcel(XlCall.xlCoerce, out object result, params object[] parameters)
+
+    switch (xlType)
+    {
+        case XlType.XlTypeArray:
+            throw new NotImplementedException("XlType.XlTypeArray");
+
+        case XlType.XlTypeBoolean:
+            if (input is bool)
+            {
+                result = input;
+                return XlCall.XlReturn.XlReturnSuccess;
+            }
+            else if (input is double || input is long)
+            {
+                result = Convert.ToBoolean(input); // Zero is False, all other values are True
+                return XlCall.XlReturn.XlReturnSuccess;
+            }
+            else
+            {
+                result = null;
+                return XlCall.XlReturn.XlReturnFailed;
+            }
+
+        case XlType.XlTypeEmpty:
+            throw new NotImplementedException("XlType.XlTypeEmpty");
+
+        case XlType.XlTypeError:
+            throw new NotImplementedException("XlType.XlTypeError");
+
+        case XlType.XlTypeInt:
+            throw new NotImplementedException("XlType.XlTypeInt");
+
+        case XlType.XlTypeMissing:
+            throw new NotImplementedException("XlType.XlTypeMissing");
+
+        case XlType.XlTypeNumber:
+            if (input is double)
+            {
+                result = input;
+                return XlCall.XlReturn.XlReturnSuccess;
+            }
+            else if (input is long || input is bool)
+            {
+                result = Convert.ToDouble(input);
+                return XlCall.XlReturn.XlReturnSuccess;
+            }
+            else if (input is string)
+            {
+                double resultDouble;
+                if (double.TryParse((string)input, out resultDouble))
+                {
+                    result = resultDouble;
+                    return XlCall.XlReturn.XlReturnSuccess;
+                }
+                else
+                {
+                    result = null;
+                    return XlCall.XlReturn.XlReturnFailed;
+                }
+            }
+            else
+            {
+                result = null;
+                return XlCall.XlReturn.XlReturnFailed;
+            }
+
+        case XlType.XlTypeReference:
+            throw new NotImplementedException("XlType.XlTypeReference");
+
+        case XlType.XlTypeString:
+            if (input is String)
+            {
+                result = input;
+                return XlCall.XlReturn.XlReturnSuccess;
+            }
+            else if (input is bool)
+            {
+                result = input.ToString().ToUpper();
+                return XlCall.XlReturn.XlReturnSuccess;
+            }
+            else if (input is double || input is long) // Also covers conversions from dates
+            {
+                result = input.ToString();
+                return XlCall.XlReturn.XlReturnSuccess;
+            }
+            else
+            {
+                result = null;
+                return XlCall.XlReturn.XlReturnFailed;
+            }
+
+        default:
+            throw new NotImplementedException("default");
+    }
+}
